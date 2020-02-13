6bfac9ac1041bb39709a7765339e707652a4fb63
Simon Shillaker <mail@simonshillaker.com>
2020-02-13 14:48:14 +0000

    Tidying up!

--- app.js
+++ app.js
@@ -21,28 +21,4 @@ app.use('/func', funcRouter);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// var createError = require('http-errors');
-// var cookieParser = require('cookie-parser');
-
-//
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-//
-
-// 404s
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// Errors
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
 module.exports = app;
